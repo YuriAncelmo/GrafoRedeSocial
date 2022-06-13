9bd635820d356203f9e1ad30b1655b73ce87ec2c
yuria <yuriancelmo@hotmail.com>
2022-06-13 19:04:50 -0300

    Adicionando os endpoints de consulta de nivel 1 e dois

--- Principal.py
+++ Principal.py
@@ -1,43 +1,43 @@
 import json
+from typing import List
 
-class Grafo:
+
+class Rede:
 
     def __init__(self):
-        #dicionario padrão
         self.grafo = {
-            'Ana':['Maria','João','Carlos','Vinicius'],
-            'Maria':['Ana','Vinicius'],
+            'Ana': ['Maria', 'João', 'Carlos', 'Vinicius'],
+            'Maria': ['Ana', 'Vinicius'],
             'Vinicius': ['Ana'],
             'Luiza': ['João'],
             'Carlos': ['Ana'],
-            'João':['Luiza']
+            'João': ['Luiza']
         }
 
-    def toJson(self):
-        return json.dumps(self,default=lambda o: o.__dict__)
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__)
 
     def adiciona_conexao(self, pessoa_a, pessoa_b):
         # adiciona na lista de adjacência
         self.grafo[pessoa_a].add(pessoa_b)
         self.grafo[pessoa_b].add(pessoa_a)
 
-    def adiciona_pessoa(self, pessoa):
+    def adiciona_pessoa(self, pessoa, conexoes):
         # adiciona na lista de adjacência
-        for conexao in pessoa.conexoes:
+        for conexao in conexoes:
             self.adiciona_conexao(pessoa, conexao)
 
     def mostra_conexao_n1(self, pessoa):
         print(f'Vizinhos de {pessoa}: {self.grafo[pessoa]}')
 
-    def mostra_conexao_n2(self,pessoa):
+    def mostra_conexao_n2(self, pessoa):
         conexao_n2 = list()
-    #Validar se com matriz de adjacência é possível diminuir a complexidade
+    # Validar se com matriz de adjacência é possível diminuir a complexidade
         for conexao in self.grafo[pessoa]:
             for possivelconexao in self.grafo[conexao]:
                 if possivelconexao not in self.grafo[pessoa] and possivelconexao != pessoa:
                     conexao_n2.append(possivelconexao)
         print(conexao_n2)
-
 
     def estado_atual(self):
         return list(self.grafo.keys())
@@ -45,3 +45,4 @@ class Grafo:
     def mostra_lista(self):
         for pessoa in self.grafo.keys():
             print(f'{pessoa}: {self.grafo[pessoa]} ', end='   ')
+
